ff007ac3bee4546b8110a6e14fd776a4c4aa0c2d
Mike Angstadt <mike.angstadt@gmail.com>
2012-10-14 16:25:30 -0400

    Minor refactorings to neaten up code.

--- web/index.php
+++ web/index.php
@@ -8,11 +8,9 @@ spl_autoload_register(function ($class) {
 	}
 });
 
-// Composer auto-loader
+// Set up Twig
 require_once __DIR__ . '/_lib/Twig/Autoloader.php';
 Twig_Autoloader::register();
-
-// Set up Twig
 $loader = new Twig_Loader_Filesystem(__DIR__ . '/_templates');
 $twig = new Twig_Environment($loader, array(
     'cache' => __DIR__ . '/twig_cache',
@@ -22,6 +20,7 @@ $twig = new Twig_Environment($loader, array(
 //$servers = array('smp1', 'smp2', 'smp3', 'smp4', 'smp5', 'smp6', 'smp7', 'smp8', 'smp9', 'utopia');
 $servers = array('smp7'); 
 
+$results = array();
 $errors = array();
 if (count($_GET) > 0){
 	//sanitize input
@@ -35,26 +34,29 @@ if (count($_GET) > 0){
 		} 
 	}
 	
+	$server = @$_GET['server'];
 	$startTime = @$_GET['startTime'];
 	$endTime = @$_GET['endTime'];
-	if ($startTime === null || $endTime === null) {
-		$errors[] = 'A start time and end time are required.';
-	} else {
-		$startTime = strtotime($startTime);
-		$endTime = strtotime($endTime);
-		if ($startTime === false || $endTime === false){
-			$errors[] = 'Invalid start/end times.';
-		} else if ($startTime > $endTime) {
-			$errors[] = 'Start time must come before end time.';
-		} else if ($endTime - $startTime > 60*60*24*7) {
-			$errors[] = 'Time span cannot be longer than 1 week.';
-		}
-	}
-
 	$x1 = @$_GET['x1'];
 	$z1 = @$_GET['z1'];
 	$x2 = @$_GET['x2'];
 	$z2 = @$_GET['z2'];
+	$player = @$_GET['player'];
+	
+	if ($startTime === null || $endTime === null) {
+		$errors[] = 'A start time and end time are required.';
+	} else {
+		$startTimeTs = strtotime($startTime);
+		$endTimeTs = strtotime($endTime);
+		if ($startTimeTs === false || $endTimeTs === false){
+			$errors[] = 'Invalid start/end times.';
+		} else if ($startTimeTs > $endTimeTs) {
+			$errors[] = 'Start time must come before end time.';
+		} else if ($endTimeTs - $startTimeTs > 60*60*24*7) {
+			$errors[] = 'Time span cannot be longer than 1 week.';
+		}
+	}
+
 	$coords = array($x1, $z1, $x2, $z2);
 	$coordsEntered = 0;
 	foreach ($coords as $coord){
@@ -84,28 +86,26 @@ if (count($_GET) > 0){
 		}
 	}
 
-	$player = @$_GET['player'];
-	
 	if (count($errors) == 0){
 		//$dao = new DbDao(getenv('DB_HOST'), getenv('DB_NAME'), getenv('DB_USER'), getenv('DB_PASS'));
 		$dao = new DbDao('localhost', 'emc_investigator', 'root', 'root', null, "/tmp/mysql.sock"); //mac
 		//$dao = new DbDao('localhost', 'emc_investigator', 'root', 'root'); //netbook
-		$results = $dao->getReadings('smp7', 'wilderness', $startTime, $endTime, $x1, $z1, $x2, $z2, $player);
+		$results = $dao->getReadings('smp7', 'wilderness', $startTimeTs, $endTimeTs, $x1, $z1, $x2, $z2, $player);
 	}
 }
 
 echo $twig->render('index.html', array(
 	'errors' => $errors,
 	'servers' => $servers,
-	'selectedServer' => @$_GET['server'],
-	'startTime' => @$_GET['startTime'],
-	'endTime' => @$_GET['endTime'],
-	'x1' => @$x1,
-	'x2' => @$x2,
-	'z1' => @$z1,
-	'z2' => @$z2,
-	'player' => @$player,
-	'results' => @$results,
+	'selectedServer' => $server,
+	'startTime' => $startTime,
+	'endTime' => $endTime,
+	'x1' => $x1,
+	'x2' => $x2,
+	'z1' => $z1,
+	'z2' => $z2,
+	'player' => $player,
+	'results' => $results,
 ));
 
 ?>
