c2e7028b965f9bff7be2735dac9b945409e6442c
Mike Angstadt <mike.angstadt@gmail.com>
2012-10-14 16:35:30 -0400

    Twig cache stuff.

--- web/index.php
+++ web/index.php
@@ -13,8 +13,9 @@ require_once __DIR__ . '/_lib/Twig/Autoloader.php';
 Twig_Autoloader::register();
 $loader = new Twig_Loader_Filesystem(__DIR__ . '/_templates');
 $twig = new Twig_Environment($loader, array(
-    'cache' => __DIR__ . '/twig_cache',
-	'auto_reload' => true, // Set to 'off' in production
+    //'cache' => __DIR__ . '/twig_cache',
+	'cache' => getenv('TWIG_CACHE'),
+	'auto_reload' => false, // Set to 'off' in production
 ));
 
 //$servers = array('smp1', 'smp2', 'smp3', 'smp4', 'smp5', 'smp6', 'smp7', 'smp8', 'smp9', 'utopia');
@@ -87,8 +88,8 @@ if (count($_GET) > 0){
 	}
 
 	if (count($errors) == 0){
-		//$dao = new DbDao(getenv('DB_HOST'), getenv('DB_NAME'), getenv('DB_USER'), getenv('DB_PASS'));
-		$dao = new DbDao('localhost', 'emc_investigator', 'root', 'root', null, "/tmp/mysql.sock"); //mac
+		$dao = new DbDao(getenv('DB_HOST'), getenv('DB_NAME'), getenv('DB_USER'), getenv('DB_PASS'));
+		//$dao = new DbDao('localhost', 'emc_investigator', 'root', 'root', null, "/tmp/mysql.sock"); //mac
 		//$dao = new DbDao('localhost', 'emc_investigator', 'root', 'root'); //netbook
 		$results = $dao->getReadings('smp7', 'wilderness', $startTimeTs, $endTimeTs, $x1, $z1, $x2, $z2, $player);
 	}
