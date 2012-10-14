0475cc1b23c162af5cee8d8ef179480b90621afe
Alexander Krivács Schrøder <alexschrod@gmail.com>
2012-10-14 10:53:51 +0200

    Converted index.php to use a Twig template instead of mixed-in HTML

--- web/index.php
+++ web/index.php
@@ -11,6 +11,13 @@ spl_autoload_register(function ($class) {
 // Composer auto-loader
 require_once __DIR__ . '/vendor/autoload.php';
 
+// Set up Twig
+$loader = new Twig_Loader_Filesystem(__DIR__ . '/twig_templates');
+$twig = new Twig_Environment($loader, array(
+    'cache' => __DIR__ . '/twig_cache',
+	'auto_reload' => true, // Set to 'off' in production
+));
+
 //$servers = array('smp1', 'smp2', 'smp3', 'smp4', 'smp5', 'smp6', 'smp7', 'smp8', 'smp9', 'utopia');
 $servers = array('smp7'); 
 
@@ -85,166 +92,18 @@ if (count($_GET) > 0){
 	}
 }
 
-?>
-<html>
-	<head>
-		<title>EMC Investigator</title>
-		
-		<!-- http://www.kryogenix.org/code/browser/sorttable/ -->
-		<script type="text/javascript" src="_js/sorttable.js"></script>
-		
-		<style>
-		body{
-			color:#fff;
-			background-color:#000;
-		}
-		
-		table{
-			color:#fff;
-		}
-		
-		tr.evenRow{
-			background-color:none;
-		}
-		
-		tr.oddRow{
-			background-color:#960;
-		}
-		
-		a.header:link, a.header:active, a.header:visited{
-			color: #fff;
-			text-decoration: none;
-		}
-		a.header:hover{
-			color: #fff;
-			text-decoration: none;
-		}
-		
-		a:link, a:active, a:visited{
-			color: #f90;
-			text-decoration: underline;
-		}
-		a:hover{
-			color: #960;
-			text-decoration: none;
-		}
-		</style>
-	</head>
-	<body>
-		<div style="position:fixed; right:10px; top:10px;">
-			<a href="http://empire.us"><img src="images/emc-logo.png" border="0" /></a><br />
-			<a href="https://sites.google.com/site/emclastlightoutpost/"><img src="images/llo-logo.png" border="0" /></a><br /><br />
-			<div style="text-align:center; font-size:0.8em; padding:20px 0px 20px 0px; background-color:#333; cursor:pointer;" onclick="window.scrollTo(0,0)">
-				[<a href="#">scroll to top</a>]
-			</div>
-		</div>
-		
-		<div style="position:fixed; right:10px; bottom:10px;">
-			<a href="https://github.com/mangstadt/emc-investigator" target="_blank" style="font-size:0.8em">http://www.github.com/mangstadt/emc-investigator</a>
-		</div>
+echo $twig->render('index.html', array(
+	'errors' => $errors,
+	'servers' => $servers,
+	'selectedServer' => @$_GET['server'],
+	'startTime' => @$_GET['startTime'],
+	'endTime' => @$_GET['endTime'],
+	'x1' => @$x1,
+	'x2' => @$x2,
+	'z1' => @$z1,
+	'z2' => @$z2,
+	'player' => @$player,
+	'results' => @$results,
+));
 
-		<a href="." class="header" style="font-size:3em; font-weight:bold;">EMC Investigator</a> <span style="color:yellow; font-size:2.0em;">[beta]</span><br />
-		<em>A griefer investigation tool for <a href="http://empire.us">Empire Minecraft</a></em><br />
-		by <a href="http://empireminecraft.com/members/shavingfoam.12110/">shavingfoam</a> (c) 2012<br />
-		<strong>Note:</strong> This tool cannot detect hidden players.
-		<hr />
-		
-		<form action="." method="get">
-			<?php
-			if ($errors){
-				?><div style="color:red"><ul><?php
-				foreach ($errors as $error){
-					echo "<li>", htmlentities($error), "</li>";
-				}
-				?></ul></div><?php
-			}
-			?>
-
-			<em><?php echo req()?> = required</em>
-			
-			<div style="padding-top:10px">
-				Server<?php echo req()?>:
-				<select name="server">
-				<?php
-				foreach ($servers as $server){
-					$selected = (@$_GET['server'] == $server) ? ' selected' : '';
-					echo "<option", $selected, ">", htmlentities($server), "</option>";
-				}
-				?>
-				</select>
-			</div>
-			
-			<div style="padding-top:10px">
-				Start time<?php echo req()?>: <input type="text" name="startTime" value="<?php echo f('startTime')?>" /> <em>(format: YYYY-MM-DD HH:MM)</em>
-			</div>
-			<div>
-				End time<?php echo req()?>: <input type="text" name="endTime" value="<?php echo f('endTime')?>" />  <em>(format: YYYY-MM-DD HH:MM)</em>
-			</div>
-			<div style="padding-top:10px">
-				Upper-left coords:<br />
-				X: <input type="text" name="x1" size="10" value="<?php echo f('x1')?>" /> Z: <input type="text" name="z1" size="10" value="<?php echo f('z1')?>" />
-			</div>
-			<div>
-				Bottom-right coords:<br />
-				X: <input type="text" name="x2" size="10" value="<?php echo f('x2')?>" /> Z: <input type="text" name="z2" size="10" value="<?php echo f('z2')?>" />
-			</div>
-			<div style="padding-top:10px">
-				Player: <input type="text" name="player" value="<?php echo f('player')?>" /> <em>(can be partial name)</em>
-			</div>
-			<input type="submit" value="Search" />
-		</form>
-		
-		<?php
-		if (isset($results)){
-			?>
-			<h1>Results</h1>
-			<div>
-				<table cellpadding="5" class="sortable" id="resultsTable">
-					<tr>
-						<th style="cursor:pointer;" id="timeCol">Time (GMT-5)</th>
-						<th style="cursor:pointer;">Player</th>
-						<th class="sorttable_nosort">(X,Z) [Y]</th>
-					</tr>
-					<?php
-					$odd = true; 
-					foreach ($results as $result){
-						foreach ($result->players as $player){
-							echo "<tr class=\"", ($odd ? 'oddRow' : 'evenRow'), "\">";
-							echo "<td>", date('Y-m-d H:i', $result->ts), "</td>";
-							echo "<td><img onerror=\"this.style.display='none'\" src=\"http://smp1.empire.us:8880/tiles/faces/16x16/" . urlencode($player->name) . ".png\" /> ", htmlentities($player->name), "</td>";
-							echo "<td>", "(", $player->x, ",", $player->z, ")", " [", $player->y, "]", "</td>";
-							echo "</tr>";
-							$odd = !$odd;
-						}
-					}
-					?>
-				</table>
-			</div>
-			<?php
-		}
-		?>
-
-		<script>
-		var table = document.getElementById('resultsTable');
-		table.afterSort = function(){
-			for (var i = 1; i < table.rows.length; i++){
-				var row = table.rows[i];
-				row.className = (i % 2 == 0) ? 'evenRow' : 'oddRow';
-			}
-		};
-		</script>
-	</body>
-</html>
-
-<?php 
-function f($name){
-	if (isset($_GET[$name])){
-		return htmlentities($_GET[$name]);
-	}
-	return '';
-}
-
-function req(){
-	return "<span style=\"color:red\">*</span>";
-}
 ?>
